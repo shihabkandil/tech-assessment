7bd65955908886282686689f1c905ce56b9de356
Shihab Kandil <79358747+shihabkandil@users.noreply.github.com>
2024-07-08 21:45:23 +0300

    feat: adaptive ui with enhanced refresh behavior (#4)
    * add adaptive grid sliver view for restaurants list
    
    * refactor context variables
    
    * add pull to refresh behavior

--- lib/features/restaurants_list/presentation/screens/restaurants_screen.dart
+++ lib/features/restaurants_list/presentation/screens/restaurants_screen.dart
@@ -2,9 +2,12 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../common/extensions/context_extensions.dart';
+import '../../../../common/constants/strings.dart';
 import '../../../../common/constants/theme/app_theme_data.dart';
 import '../../../../common/constants/theme/app_colors.dart';
 import '../../../../common/widgets/loading_widget.dart';
+import '../../../../gen/assets.gen.dart';
 import '../cubits/restaurants_cubit.dart';
 import '../widgets/restaurant_card.dart';
 
@@ -22,52 +25,62 @@ class RestaurantsScreen extends StatelessWidget {
           appBar: AppBar(
             backgroundColor: Colors.white,
             centerTitle: false,
-            title: Image.asset(
-              "assets/images/logo.png",
+            title: Assets.images.logo.image(
               width: 90,
               height: 150,
             ),
           ),
           body: Padding(
             padding: const EdgeInsets.all(14),
-            child: CustomScrollView(
-              slivers: [
-                SliverPadding(
-                  padding: const EdgeInsets.all(8),
-                  sliver: SliverToBoxAdapter(
-                    child: Text(
-                      'Our Restaurants',
-                      style: context.textTheme.titleLarge
-                          ?.copyWith(color: AppColors.black),
+            child: RefreshIndicator(
+              onRefresh: () async =>
+                  context.read<RestaurantsCubit>().getRestaurants(),
+              child: CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding: const EdgeInsets.all(8),
+                    sliver: SliverToBoxAdapter(
+                      child: Text(
+                        Strings.ourRestaurants,
+                        style: context.textTheme.titleLarge
+                            ?.copyWith(color: AppColors.black),
+                      ),
                     ),
                   ),
-                ),
-                BlocBuilder<RestaurantsCubit, RestaurantsState>(
-                  builder: (context, state) {
-                    return state.maybeWhen(
-                      orElse: () {
-                        return const SliverToBoxAdapter(
-                          child: LoadingWidget(),
-                        );
-                      },
-                      success: (restaurants) => SliverPadding(
-                        padding: const EdgeInsets.all(8),
-                        sliver: SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (context, index) {
-                              final restaurant = restaurants[index];
-                              return RestaurantCard(
-                                restaurant: restaurant,
-                              );
-                            },
-                            childCount: restaurants.length,
+                  BlocBuilder<RestaurantsCubit, RestaurantsState>(
+                    builder: (context, state) {
+                      return state.maybeWhen(
+                        orElse: () {
+                          return const SliverToBoxAdapter(
+                            child: LoadingWidget(),
+                          );
+                        },
+                        success: (restaurants) => SliverPadding(
+                          padding: const EdgeInsets.all(8),
+                          sliver: SliverGrid(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: context.isMobile ? 1 : 2,
+                              mainAxisSpacing: 12,
+                              crossAxisSpacing: 12,
+                              childAspectRatio: 3 / 1,
+                            ),
+                            delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                                final restaurant = restaurants[index];
+                                return RestaurantCard(
+                                  restaurant: restaurant,
+                                );
+                              },
+                              childCount: restaurants.length,
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         );
