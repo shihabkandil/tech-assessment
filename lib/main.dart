a8d109717e8c9ab2a1791b49b14cf672da253cab
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-07-08 20:50:49 +0300

    add restaurants screen routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'common/constants/theme/app_theme_data.dart';
+import 'common/core/injector/locator.dart';
 import 'common/core/router/app_router.dart';
 
 void main() {
+
+  configureDependencies();
+
   runApp(
     App(appRouter: AppRouter()),
   );
@@ -17,7 +21,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: AppThemeData.lightTheme,
-      routerConfig: appRouter.config(),
       routerDelegate: appRouter.delegate(),
       routeInformationParser: appRouter.defaultRouteParser(),
     );
